78c5c9b512f7445b8cc2f8f4c232d38aed237597
Lazuardi Naufal <lazuardi.n@samsung.com>
2026-01-07 13:41:41 +0700

    add create opencloud folder

--- OpenCloud.psm1
+++ OpenCloud.psm1
@@ -72,3 +72,28 @@ function Set-OpenCloudItem {
     }
     catch { if ($silent) { return $false } else { throw } }
 }
+
+function Set-OpenCloudDirectory {
+    [OutputType([bool])]
+    param (
+        [Alias("OpenCloudUrl")] [ValidateNotNullOrWhiteSpace()] [string]$url,
+        [Alias("DirectoryPath")] [ValidateNotNullOrWhiteSpace()] [string]$path,
+        [Alias("UserId")] [ValidateNotNullOrWhiteSpace()] [string]$id,
+        [Alias("UserPassword")] [ValidateNotNullOrWhiteSpace()] [string]$pass,
+        [Alias("SpaceId")] [string]$space,
+        [Alias("OnErrorContinue")] [switch]$silent
+    )
+    try {
+        if (!(Find-OpenCloudItem $url $path $id $pass $space -BaseItemOnly -OnErrorContinue)) {
+            $rest = @{
+                Uri = $space ? "$url/dav/spaces/$space/$path" : "$url/remote.php/dav/files/$id/$path"
+                CustomMethod = "MKCOL"
+                Authentication = "Basic"
+                Credential = [PSCredential]::new($id, (ConvertTo-SecureString $pass -AsPlainText -Force))
+            }
+            Invoke-RestMethod @rest | Out-Null
+        }
+        return $true
+    }
+    catch { if ($silent) { return $false } else { throw } }
+}
